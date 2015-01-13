5a634de9ea61404a87f95bd5b418996d9f001efb
kknevitt <knev123@hotmail.com>
2015-01-13 22:51:58 +0000

    refactored into case statement

--- calculator.rb
+++ calculator.rb
@@ -1,29 +1,49 @@
 Shoes.app :title => "My Slightly Broken Calculator", :width => 200, :height => 240 do
-  stack :margin => 55 do
-    @output = edit_line width: 100
+  stack :margin => 30 do
+    @output = edit_line width: 125
 
     
     flow do
-      %w(9 8 7 6 5 4 3 2 1 0 + / * ( )- ).each do |op|      
-        button op do         
-          append op
+      %w( 9 8 7 6 5 4 3 2 1 0 + % * . - = ^2 c sqrt log2).each do |op|      
+        button op do
+          case op
+            when "=" 
+              eval_expression
+            when "c"
+              @input = ""
+              @output.text = ""
+            when "^2"
+              append "**2"
+            when "sqrt"
+              @input = Math.sqrt(@input.to_f).to_s
+              @output.text = @input
+            when "log2"
+               @input = Math.log2(@input.to_f).to_s
+               @output.text = @input
+             when "%"
+              append "/"
+            else 
+              append op
+            end
         end
       end
       
-      button "=" do
-        eval_expression
-      end
+      # button "=" do
+      #   eval_expression
+      # end
 
+=begin
       button "c" do
         @input = ""
         @output.text = ""
       end
+=end
 
-      button "^2" do
-        append "**2"
-      end
+      # button "^2" do
+      #   append "**2"
+      # end
 
-      button "sqrt" do
+=begin      button "sqrt" do
 
         # @inputInt  = @input.to_i
         # @sqrt = Math.sqrt(@inputInt)
@@ -38,7 +58,7 @@ Shoes.app :title => "My Slightly Broken Calculator", :width => 200, :height => 2
        @input = Math.log2(@input.to_i).to_s
        @output.text = @input
       end
-
+=end
     end
     
   end
