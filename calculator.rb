d5c9ebd3a31fb274a362877859142da54f528163
Lyra-B <glykeriapeppa@gmail.com>
2015-01-14 13:44:07 +0000

    CaseStatementScientificFunctionsDivisionSign

--- calculator.rb
+++ calculator.rb
@@ -1,44 +1,66 @@
-Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 240 do
-  @back  = background blue
+# encoding: UTF-8
+Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 280 do
+  @back  = background darkslateblue
   stack :margin => 10 do
-    
-    @back  = background blue
+    @back  = background darkslateblue
     @output = edit_line width:166, margin:4
     
     flow do
-      %w(7 8 9 + 4 5 6 - 1 2 3 / 0 *).each do |op|      
-        button op do         
-          append op
+      %w(7 8 9 + 4 5 6 - 1 2 3 ÷ 0 = C * ** sqrt log2).each do |op|      
+        button op do  
+          case op
+            when "="
+              eval_expression
+            when "C"
+              @output.text = nil
+              @input = nil
+            when "**"
+              append "**2"
+            when "÷"
+              append "/"
+            when "sqrt"
+              @input = Math.sqrt(@input.to_f).to_s
+              @output.text =@input
+            when "log2"
+              @input = Math.log2(@input.to_i).to_s
+              @output.text =@input
+            else       
+              append op
+          end
         end
       end
       
-      button "=" do
-        eval_expression
-      end
-
-      button "C" do
-        @output.text = nil
-        @input = nil
-      end
-
-      button "**" do 
-        append "**2"
-        eval_expression
-      end
-
-      # button "÷" do 
-      #   append "/"
+      # button "=" do
+      #   eval_expression
       # end
+
+      # button "C" do
+      #   @output.text = nil
+      #   @input = nil
+      # end
+
+      # button "**" do 
+      #   append "**2"
+      #   eval_expression
+      # end
+
+      # # @input = Math.sqrt(@input.to_f).to_s
+      # # @output.text =@input
+
+      # # button "÷" do 
+      # #   append "/"
+      # # end
     
+      # # button "√" do
+      # #   Math.sqrt(@output)
+      # #   eval_expression
+      # # end
+
+      # # button "log2" do
+      # #   Math.log2(@output.text)
+      # # end
     end
   end
-
-      # button "√" do
-      # end
-
-      # button "log2" do
-      #   Math.log2(@output.text)
-      # end
   
   # Stick a string on the end of our input
   #
