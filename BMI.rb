5117372ff24def9df88246e0e13a060a370e172d
dansteele <danielsteele@hotmail.co.uk>
2015-01-24 13:54:19 +0000

    Changed 'magic number' to show inches to meters

---
+++ BMI.rb
@@ -0,0 +1,56 @@
+Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
+  stack :margin => 20 do
+    @output = edit_line
+
+    @output.text = "Enter your height"
+
+    stack :width => 80 do
+      flow do
+
+        %w(7 8 9 4 5 6 1 2 3 . C m inches).each do |op|      
+          button op do
+            case op
+              when "m"
+                @height = (@input.to_f**2)
+                puts @height
+                ask_for_weight
+              when "inches"
+                inches_to_meters = 0.0254
+                @height = ((inches_to_meters*(@input.to_f))**2)
+                puts @height
+                ask_for_weight
+              when "C"
+                @input = ""
+                @output.text = " "
+              else
+              append op
+            end
+          end
+        end
+      end
+    end
+  end
+
+
+  def ask_for_weight
+    @output.text = "Enter weight."
+    @input = ""
+
+    button "kg" do
+      @input = (@input.to_f / @height).to_s
+      @output.text = @input
+    end
+
+    button "lbs" do
+      @input = ((@input.to_f*0.453592) / @height).to_s
+      @output.text = @input
+    end
+  end
+
+  def append(s)
+    @input ||= ""
+    @input += s
+    @output.text = @input
+  end
+
+end
--- calculator.rb
+++ calculator.rb
@@ -1,40 +1,40 @@
 Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
   stack :margin => 20 do
     @output = edit_line
-    
-    flow do
-      %w(0 1 2 3 4 5 6 7 8 9 + / * -).each do |op|      
-        button op do         
-          append op
+
+    stack :width => 80 do
+      flow do
+        %w(7 8 9 4 5 6 1 2 3 . + / * - = C ^2 sqrt log(2) Miles\ to\ KM KM\ to\ Miles).each do |op|      
+          button op do
+            case op
+              when "="
+                eval_expression
+              when "C"
+                @input = ""
+                @output.text = " "
+              when "^2"
+                append "**2"
+              when "sqrt"
+                @input = Math.sqrt(@input.to_f).to_s
+                @output.text = @input
+              when "log(2)"
+                @input = Math.log2(@input.to_f).to_s
+                @output.text = @input
+              when "Miles to KM"
+                @input = (1.609344*(@input.to_f)).to_s
+                @output.text = @input
+              when "KM to Miles"
+                @input = (0.621371192*(@input.to_f)).to_s
+                @output.text = @input 
+            else         
+            append op
+          end
+          end
         end
       end
-      
-      button "=" do
-        eval_expression
-      end
-
-      button "C" do
-        @input = ""
-        @output.text = " "
-      end
-
-      button "^2" do
-        append "**2"
-      end
-
-      button "sqrt" do
-        @input = Math.sqrt(@input.to_i).to_s
-        @output.text = @input
-      end
-
-      button "log(2)" do
-        @input = Math.log2(@input.to_i).to_s
-        @output.text = @input
-      end
-
     end
   end
-  
+
   # Stick a string on the end of our input
   #
   def append(s)
