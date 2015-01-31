c0911afdf8aa47d9b55a2e4851a513d3c0cbea70
Lyra-B <glykeriapeppa@gmail.com>
2015-01-31 18:09:24 +0000

    Fixed Float Problem

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,12 @@
 # encoding: UTF-8
-Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 280 do
-  @back  = background darkslateblue
+Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 240 do
+  @back  = background black
   stack :margin => 10 do
-    @back  = background darkslateblue
+    @back  = background black
     @output = edit_line width:166, margin:4
     
     flow do
-      %w(7 8 9 + 4 5 6 - 1 2 3 ÷ 0 = C * ** sqrt log2).each do |op|      
+      %w(7 8 9 + 4 5 6 - 1 2 3 ÷ 0 = C * ^ sqrt log2).each do |op| 
         button op do  
           case op
             when "="
@@ -14,8 +14,9 @@ Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 280 do
             when "C"
               @output.text = nil
               @input = nil
-            when "**"
+            when "^"
               append "**2"
+              eval_expression
             when "÷"
               append "/"
             when "sqrt"
@@ -29,36 +30,6 @@ Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 280 do
           end
         end
       end
-      
-      # button "=" do
-      #   eval_expression
-      # end
-
-      # button "C" do
-      #   @output.text = nil
-      #   @input = nil
-      # end
-
-      # button "**" do 
-      #   append "**2"
-      #   eval_expression
-      # end
-
-      # # @input = Math.sqrt(@input.to_f).to_s
-      # # @output.text =@input
-
-      # # button "÷" do 
-      # #   append "/"
-      # # end
-    
-      # # button "√" do
-      # #   Math.sqrt(@output)
-      # #   eval_expression
-      # # end
-
-      # # button "log2" do
-      # #   Math.log2(@output.text)
-      # # end
     end
   end
   
@@ -74,7 +45,12 @@ Shoes.app :title => "My Amazing Calculator", :width => 189, :height => 280 do
   
   # Evaluate the input we've got so far
   def eval_expression
-    @input = eval(@input).to_s
+    string_arr = @input.split(/([\/*+\^-])/)
+    string_arr.map! do |k|
+      k.match(/[\/*+\^-]/) ? k : k.to_f
+    end
+    @input = string_arr.join
+    @input = eval(@input)
     @output.text = @input
   end
 end
